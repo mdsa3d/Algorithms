a7f77c45327c1457ebe0347ec6bc27718338336c
Mohammad Saad <saadsiraj254@gmail.com>
2021-07-04 23:57:07 +0800

    using parsing to extract data

---
+++ Julia/JSON/conv-vec-matrix.jl
@@ -0,0 +1,14 @@
+x = [1,2,3,4,5,6]
+x_s ="[3, 2]"
+
+b = parse.(Int64, split(strip(x_s, ['[',']']), ','))
+println("b is $b")
+
+a = b[1]
+e = b[2]
+
+println("a is $a e is $e")
+
+
+x_new = transpose(reshape(x,a,e))
+println(x_new)
